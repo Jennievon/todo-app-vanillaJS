98f84f0cf582ee5199b601a0c69a40fe3ce7de1d
Jennifer <floxzyjay@gmail.com>
2019-08-28 17:35:59 +0100

    changed position of 'delete'

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-var form = document.getElementById('form'),
-  todoTitle = document.getElementById('todoTitle')
-todoDetails = document.getElementById('todoDetails')
-savedTodos = document.getElementById('savedTodos');
+const todos = document.querySelector('#todos'),
+  form = document.querySelector('form'),
+  newItem = document.querySelector('#newItem');
 
+form.addEventListener('submit',
+  function (event) {
+    event.preventDefault();
+    todos.innerHTML += '<li>' + newItem.value + '</li>';
+    store();
+    newItem.value = "";
+  }, false);
 
-//form.addEventListener('submit', addTodo);
-function addTodo(event) {
-  event.preventDefault();
-  var todo = {}
-
-  todo.title = todoTitle.value
-  todo.details = todoDetails.value
-
-  console.log(todo)
-
-  if (localStorage.getItem('todos') == null) {
-    var todo = [];
-    todo.push('todo');
-    localStorage.setItem('todo', JSON.stringify(todo));
-  } else {
-    var myTodo = localStorage.getItem(todo, JSON.parse('todo'));
-    myTodo.push('todo');
-    localStorage.setItem('todo', JSON.stringify(myTodo));
-  }
-}
-
-function loadApp() {
-  var todo = JSON.parse(localStorage.getItem('todo'));
-  savedTodos.innerHTML = "";
-  if (localStorage.getItem('todos') !== null) {
-    for (let i = 0; i < todo.length; i++) {
-      const title = todo[i].title;
-      const details = todo[i].details;
-      savedTodos.innerHTML += '<div> <h4>' + title + ' ' + details + ' </h4> </div>'
+todos.addEventListener('click',
+  function (e) {
+    let todo = e.target;
+    if (todo.classList.contains('checked')) {
+      todo.parentNode.removeChild(todo);
     }
-  }
+    else {
+      todo.classList.add('checked');
+    }
+    store();
+  }, false);
 
+function store() {
+  window.localStorage.myitems = todos.innerHTML;
 }
+
+function addTodos() {
+  var todoStored = window.localStorage.myitems;
+  if (!todoStored) {
+    todos.innerHTML = '<li>Park the car</li>' +
+      '<li>Mow the lawn</li>' +
+      '<li>Study</li>';
+  } else {
+    todos.innerHTML = todoStored;
+  }
+}
+addTodos();
